3a57c2d00fba4b87003331fbbfd411a1a47ecfd6
Maxim <maxsimus97.ivanov@yandex.ru>
2023-04-20 17:56:41 +0300

    add asciinema in README.md and fix App, Cli and GameEven files

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/toujoursMaxim/java-project-61/workflows/hexlet-check/badge.svg)](https://github.com/toujoursMaxim/java-project-61/actions)
 [![Test Coverage](https://api.codeclimate.com/v1/badges/36c9cb475475669af905/test_coverage)](https://codeclimate.com/github/toujoursMaxim/java-project-61/test_coverage)
+
+win
+https://asciinema.org/a/ThxeMSrNlSoLYGzToXL6Ie3id
+lose
+https://asciinema.org/a/I35Zc0gMt1LPIIGXM4iq6FNar
--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,9 +1,29 @@
 package hexlet.code;
 
+import java.util.Scanner;
+
 public class App {
 
     public static void main(String[] args) {
         System.out.println("Welcome to the Brain Games!");
-        Cli.greeting();
+        System.out.println("""
+        Please enter the game number and press Enter.
+        1 - Greet
+        2 - Game Even
+        3 - Calc
+        0 - Exit""");
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+        System.out.println("Your choice: " + number);
+        switch (number) {
+            case (0):
+                break;
+            case (1):
+                Cli.greeting();
+                break;
+            case (2):
+                GameEven.even();
+                break;
+        }
     }
 }
