62f6988debda9c5c4a1c17bd7253d42acc5dbeb4
Maxim <maxsimus97.ivanov@yandex.ru>
2023-04-20 15:40:11 +0300

    add GameEven and fix App, Cli and add asciinema in README.md

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/toujoursMaxim/java-project-61/workflows/hexlet-check/badge.svg)](https://github.com/toujoursMaxim/java-project-61/actions)
-[![Test Coverage](https://api.codeclimate.com/v1/badges/36c9cb475475669af905/test_coverage)](https://codeclimate.com/github/toujoursMaxim/java-project-61/test_coverage)
+[![Test Coverage](https://api.codeclimate.com/v1/badges/36c9cb475475669af905/test_coverage)](https://codeclimate.com/github/toujoursMaxim/java-project-61/test_coverage
+https://asciinema.org/a/Zo2XhPoJxFYgdLxgjBuEzPr8C)
--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,9 +1,27 @@
 package hexlet.code;
 
+import java.util.Scanner;
+
 public class App {
 
     public static void main(String[] args) {
-        System.out.println("Welcome to the Brain Games!");
-        Cli.greeting();
+        System.out.println("""
+        Please enter the game number and press Enter.
+        1 - Greet
+        2 - Game Even
+        0 - Exit""");
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+        System.out.println("Your choice is " + number);
+        switch (number) {
+            case (0):
+                break;
+            case (1):
+                Cli.greeting();
+                break;
+            case (2):
+                GameEven.even();
+                break;
+        }
     }
 }
