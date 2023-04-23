57ef51b7b35608765c946509e7a0ca281aa4ad3a
Maxim Iva <66096338+toujoursMaxim@users.noreply.github.com>
2023-04-23 20:59:07 +0300

    Update GameEven.java

--- app/src/main/java/hexlet/code/GameEven.java
+++ app/src/main/java/hexlet/code/GameEven.java
@@ -10,23 +10,17 @@ public class GameEven {
 
         System.out.println();
         System.out.println("Welcome to the Brain Games!");
-<<<<<<< HEAD
         System.out.print("May I have your name? ");
         String userName = scanner.nextLine();
         System.out.println("Hello, " + userName + "!");
-=======
->>>>>>> refs/remotes/origin/main
         System.out.println("Answer 'yes' if the number is even, otherwise answer 'no'.");
 
         for (int i = 0; i <= 2; i++) {
             int number = random.nextInt(100);
             System.out.println("Question: " + number);
-
-<<<<<<< HEAD
             System.out.print("Your answer: ");
-=======
+
             System.out.println("Your answer: ");
->>>>>>> refs/remotes/origin/main
             String answer = scanner.nextLine();
             String correctAnswer = (number % 2 == 0) ? "yes" : "no";
 
@@ -34,19 +28,14 @@ public class GameEven {
                 System.out.println("Correct!");
             } else {
                 System.out.println("'" + answer + "'" + " is wrong answer. ;( " +
-<<<<<<< HEAD
                         "Correct answer was " + "'" + correctAnswer + "'" + ".\nLet's try again, " + userName + "!");
                 break;
+
             }
             if (i == 2) {
                 System.out.println("Congratulations, " + userName + "!");
-=======
-                        "Correct answer was " + "'" + correctAnswer + "'" + ".\nLet's try again!");
                 break;
-            }
-            if (i == 2) {
-                System.out.println("Congratulations!");
->>>>>>> refs/remotes/origin/main
+                }
             }
         }
     }
