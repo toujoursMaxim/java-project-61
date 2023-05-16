c24adae3f3e52db1389d629ffac5b59abfb1a57f
Maxim <maxsimus97.ivanov@yandex.ru>
2023-05-16 11:05:42 +0300

    update Utils.java

--- app/src/main/java/hexlet/code/Utils.java
+++ app/src/main/java/hexlet/code/Utils.java
@@ -4,4 +4,8 @@ public class Utils {
     public static int getRandomNumber(int min, int max) {
         return min + (int) (Math.random() * max);
     }
+
+    public static int getPosition(int length) {
+        return (int) (Math.random() * length);
+    }
 }
